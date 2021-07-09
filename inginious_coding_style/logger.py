c725241530c505c9b761b131c867d651aa2978fb
PederHA <peder.andresen@gmail.com>
2021-07-09 17:29:53 +0200

    Add logger formatting, use functools.cache if >=py3.9

--- inginious_coding_style/logger.py
+++ inginious_coding_style/logger.py
@@ -1,7 +1,26 @@
-from logging import Logger, getLogger
-from functools import lru_cache
+import logging
+import sys
+
+try:
+    # try to use Python 3.9's cache decorator
+    # TODO: test on python3.9
+    from functools import cache  # type: ignore
+except ImportError:
+    from functools import lru_cache, partial
+
+    cache = partial(lru_cache, None)
 
 
-@lru_cache(None)
-def get_logger() -> Logger:
-    return getLogger("inginious.plugins.inginious_coding_style")
+@cache()
+def get_logger() -> logging.Logger:
+    # We just copy the logging style of INGInious here, because
+    # plugins load before the standard loggers are instantiated.
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger = logging.getLogger("inginious.plugins.inginious_coding_style")
+    logger.addHandler(ch)
+    return logger
