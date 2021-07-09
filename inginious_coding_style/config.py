ed4a4805e8f4d60da654999ab8ab2cc2922e7a77
PederHA <peder.andresen@gmail.com>
2021-07-09 17:31:39 +0200

    Add logging of invalid value removal

--- inginious_coding_style/config.py
+++ inginious_coding_style/config.py
@@ -13,21 +13,25 @@ class PluginConfig(BaseModel):
 
     @validator("enabled", pre=True)
     def validate_enabled_pre(cls, enabled: Any, field: ModelField) -> Any:
-        """Handles empty input without having to declare enabled as Optional[]"""
+        """Handles empty input without having to declare `enabled` as Optional[]"""
         if enabled is None:
             return []
         return enabled
 
     @validator("enabled")
     def validate_enabled(cls, enabled: List[str], field: ModelField) -> List[str]:
-        """Validates values in list of enabled categories."""
+        """Validates values in list of enabled grading categories."""
+        # Remove invalid options
+        logger = get_logger()
         for val in list(enabled):
             if val not in _categories:
                 enabled.remove(val)
+                logger.debug(
+                    f"Removing invalid config value '{val}' for parameter 'enabled'."
+                )
         if not enabled:
-            logger = get_logger()
             logger.warning(
-                "INGInious Coding Style: No valid values received for option 'enabled'. "
+                "No valid values received for option 'enabled'. "
                 f"Falling back on defaults: {_categories}"
             )
             enabled = _categories
